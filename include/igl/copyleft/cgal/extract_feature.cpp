e4f516ea4128d01072b256e336739fbdf3e2fdfe
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2018-09-19 16:03:48 +0200

    Missing #include for igl::PI

--- include/igl/copyleft/cgal/extract_feature.cpp
+++ include/igl/copyleft/cgal/extract_feature.cpp
@@ -1,13 +1,14 @@
 // This file is part of libigl, a simple c++ geometry processing library.
-// 
+//
 // Copyright (C) 2016 Qingnan Zhou <qnzhou@gmail.com>
-// 
-// This Source Code Form is subject to the terms of the Mozilla Public License 
-// v. 2.0. If a copy of the MPL was not distributed with this file, You can 
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
 // obtain one at http://mozilla.org/MPL/2.0/.
 
 #include "extract_feature.h"
 #include <igl/unique_edge_map.h>
+#include <igl/PI.h>
 #include <CGAL/Kernel/global_functions.h>
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 
